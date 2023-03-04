28f64cc77a5935a531e6b3b84b25e19f3d71e785
MauricioAlonso <program.alonso@gmail.com>
2023-03-04 16:26:36 -0300

    probando link

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ function crearColumna(patriota) {
 
 function detallePatriota(codigo) {
   console.log(codigo);
-  console.log(window.location.origin + "./pages/cv.html?codigo=" + codigo);
+  console.log(window.location.origin + "./patriotas/pages/cv.html?codigo=" + codigo);
   window.location.href = window.location.origin + "/pages/cv.html?codigo=" + codigo;
 }
 
